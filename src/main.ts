84ecaecd35ff126ef27b281cc4c4583503823fb1
Gabriel-Mig <gabrielmmoura2008@gmail.com>
2025-08-28 16:56:32 -0300

    enviando classe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import Aluno from "./Aluno.js";
+//import Aluno from "./Aluno.js";
+import Carro from "./Carro.js";
 
-let aluno1: Aluno = new Aluno("Felisberto Felis", 20251234, "Economia");
-let aluno2: Aluno = new Aluno("Roberta Campos", 20259876, "Educação Física");
+let carro1: Carro = new Carro("Ford Mustang", 1975, "Preto");
 
-aluno1.estudar("Juros compostos");
-aluno2.estudar("Anatomia");
+let ligado: boolean = carro1.ligar();
 
-aluno1.entregarAtividade();
-aluno2.entregarAtividade();
+if(ligado) {
+    carro1.acelerar(200);
+} else {
+    console.log ("O carro não está ligado.");
+}
+
+//let aluno1: Aluno = new Aluno("Felisberto Felis", 20251234, "Economia");
+//let aluno2: Aluno = new Aluno("Roberta Campos", 20259876, "Educação Física");
+
+//aluno1.estudar("Juros compostos");
+//aluno2.estudar("Anatomia");
+
+//aluno1.entregarAtividade();
+//aluno2.entregarAtividade();
